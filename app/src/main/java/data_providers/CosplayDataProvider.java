56df516c722e6ebdabefcf54ec1e87b3f04a1054
AntosMacek <mawinbm@gmail.com>
2016-08-01 16:14:35 +0300

    Crashing on cosplay

---
+++ app/src/main/java/data_providers/CosplayDataProvider.java
@@ -0,0 +1,157 @@
+package data_providers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by antos on 8/1/16.
+ */
+public class CosplayDataProvider {
+
+    public static HashMap<String, List<String>> getInfo() {
+        HashMap<String, List<String>> cosplayDetails = new HashMap<>();
+        List<String> DayOneBlockOne = new ArrayList<>();
+
+        DayOneBlockOne.add("V Dosku");
+        DayOneBlockOne.add("Medovaya Hanna");
+        DayOneBlockOne.add("Parva and Hina");
+        DayOneBlockOne.add("Rangi");
+        DayOneBlockOne.add("Yozhik and Namie");
+        DayOneBlockOne.add("Beresklet");
+        DayOneBlockOne.add("Nikrofil and MiAuta");
+        DayOneBlockOne.add("Jokertina");
+        DayOneBlockOne.add("Alsy");
+        DayOneBlockOne.add("Fury and Paranoya");
+        DayOneBlockOne.add("Lady Wolf");
+        DayOneBlockOne.add("Kai Urusai and Nia");
+        DayOneBlockOne.add("MasterAGB, SkyRocker, Coraline");
+        DayOneBlockOne.add("Foxes in the fog");
+        DayOneBlockOne.add("Sakura");
+        DayOneBlockOne.add("Mr. Wolf");
+        DayOneBlockOne.add("Alex and The great Martin-san");
+        DayOneBlockOne.add("Yami");
+        DayOneBlockOne.add("Eminora");
+        DayOneBlockOne.add("Horomi and Ulavita");
+        DayOneBlockOne.add("Yoon Sora");
+        DayOneBlockOne.add("AzyoMecha");
+        DayOneBlockOne.add("Mintaka and Koneko-chan");
+        DayOneBlockOne.add("Rae");
+        DayOneBlockOne.add("Kuroko, May Kim, Kodokuhime");
+        DayOneBlockOne.add("Vental");
+        DayOneBlockOne.add("John and Tanuki");
+        DayOneBlockOne.add("Flafstar");
+        DayOneBlockOne.add("Silverwood");
+        DayOneBlockOne.add("Andosmir");
+        DayOneBlockOne.add("Elyanna and Alekas");
+
+        List<String> DayOneBlockTwo = new ArrayList<>();
+
+        DayOneBlockTwo.add("Simatian");
+        DayOneBlockTwo.add("Dragons of Destiny");
+        DayOneBlockTwo.add("Boltushka");
+        DayOneBlockTwo.add("Insomnia");
+        DayOneBlockTwo.add("Zaulinio");
+        DayOneBlockTwo.add("Kuroko, May Kim, Kodokuhime");
+        DayOneBlockTwo.add("Cere");
+        DayOneBlockTwo.add("Arizona");
+        DayOneBlockTwo.add("BLAST");
+        DayOneBlockTwo.add("Sanahi");
+        DayOneBlockTwo.add("Dragons of Destiny");
+        DayOneBlockTwo.add("Hope");
+        DayOneBlockTwo.add("Insomnia");
+        DayOneBlockTwo.add("Vanilla Cat");
+        DayOneBlockTwo.add("T-oku and Visual Aya");
+        DayOneBlockTwo.add("Vega, Gen, Eztly Tigerlehik");
+        DayOneBlockTwo.add("Amirah Odette");
+        DayOneBlockTwo.add("Mei");
+        DayOneBlockTwo.add("Foxtail");
+        DayOneBlockTwo.add("Commander Kokoro");
+        DayOneBlockTwo.add("Guts and Sighter");
+        DayOneBlockTwo.add("Sanahi, Dulcinea, Rae");
+        DayOneBlockTwo.add("Tanuki");
+        DayOneBlockTwo.add("White raven");
+        DayOneBlockTwo.add("Mintaka");
+        DayOneBlockTwo.add("MO and Ralina");
+        DayOneBlockTwo.add("John");
+        DayOneBlockTwo.add("Aiko");
+        DayOneBlockTwo.add("Flafstar");
+
+        List<String> DayTwoBlockThree = new ArrayList<>();
+
+        DayTwoBlockThree.add("Tanagashi");
+        DayTwoBlockThree.add("Natsumi and Dinarin");
+        DayTwoBlockThree.add("Beresklet");
+        DayTwoBlockThree.add("Gauzera and Luka");
+        DayTwoBlockThree.add("Visual Aya");
+        DayTwoBlockThree.add("Zaulinio");
+        DayTwoBlockThree.add("Lilith");
+        DayTwoBlockThree.add("Pandemonnium");
+        DayTwoBlockThree.add("Svajone");
+        DayTwoBlockThree.add("Insomnia");
+        DayTwoBlockThree.add("Arizona");
+        DayTwoBlockThree.add("Dulcinea");
+        DayTwoBlockThree.add("Diva-de-Kara");
+        DayTwoBlockThree.add("Vega");
+        DayTwoBlockThree.add("City Boyz");
+        DayTwoBlockThree.add("Kira and Ice Sakura");
+        DayTwoBlockThree.add("Rhaine");
+        DayTwoBlockThree.add("Thanks for Smoking");
+        DayTwoBlockThree.add("Boltushka");
+        DayTwoBlockThree.add("Lin, Little Lady, Little Lord, Dictamnus, Chibi");
+        DayTwoBlockThree.add("Fury and Paranoya");
+        DayTwoBlockThree.add("Kuroi Tsubasa");
+        DayTwoBlockThree.add("Thanks for Smoking");
+        DayTwoBlockThree.add("Koneko-chan");
+        DayTwoBlockThree.add("Guren no harem");
+        DayTwoBlockThree.add("CaptainMort");
+        DayTwoBlockThree.add("Kai Urusai and Nia");
+        DayTwoBlockThree.add("SHIRO I");
+        DayTwoBlockThree.add("Lady Wolf");
+        DayTwoBlockThree.add("Fairy World");
+        DayTwoBlockThree.add("Shapeshifter");
+        DayTwoBlockThree.add("Blackout");
+
+        List<String> DayTwoBlockFour = new ArrayList<>();
+
+        DayTwoBlockFour.add("Nice-kun");
+        DayTwoBlockFour.add("Zaulinio");
+        DayTwoBlockFour.add("Visual Aya and Crossmant");
+        DayTwoBlockFour.add("Scarlett Lily");
+        DayTwoBlockFour.add("Insomnia");
+        DayTwoBlockFour.add("Black&White");
+        DayTwoBlockFour.add("John and Kato");
+        DayTwoBlockFour.add("Hooots_Senpai");
+        DayTwoBlockFour.add("Narya");
+        DayTwoBlockFour.add("Sanahi");
+        DayTwoBlockFour.add("Kiom and Drebedenik");
+        DayTwoBlockFour.add("SkyRocker, Coraline, Templars");
+        DayTwoBlockFour.add("HELL");
+        DayTwoBlockFour.add("T-oku and Wolfsgirl");
+        DayTwoBlockFour.add("Miyabi and Ivo");
+        DayTwoBlockFour.add("Heroes of Honor");
+        DayTwoBlockFour.add("Insomnia");
+        DayTwoBlockFour.add("Anika");
+        DayTwoBlockFour.add("White Flame");
+        DayTwoBlockFour.add("Dragons of Destiny");
+        DayTwoBlockFour.add("Vikonija");
+        DayTwoBlockFour.add("Vental and Mei");
+        DayTwoBlockFour.add("John");
+        DayTwoBlockFour.add("Cat Bounce");
+        DayTwoBlockFour.add("Kaori and Margosha");
+        DayTwoBlockFour.add("Nashiro");
+        DayTwoBlockFour.add("BLAST");
+        DayTwoBlockFour.add("Flafstar and MadMaid");
+        DayTwoBlockFour.add("True Baba");
+        DayTwoBlockFour.add("Lasura");
+
+        cosplayDetails.put("Day 1 Block 1", DayOneBlockOne);
+        cosplayDetails.put("Day 1 Block 2", DayOneBlockTwo);
+        cosplayDetails.put("Day 2 Block 3", DayTwoBlockThree);
+        cosplayDetails.put("Day 2 Block 4", DayTwoBlockFour);
+
+        return cosplayDetails;
+
+    }
+
+}
--- app/src/main/java/uncon/lv/uniapp/activities/CosplayActivity.java
+++ app/src/main/java/uncon/lv/uniapp/activities/CosplayActivity.java
@@ -1,7 +1,14 @@
 package uncon.lv.uniapp.activities;
 
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import data_providers.CosplayAdapter;
+import data_providers.CosplayDataProvider;
 import uncon.lv.uniapp.R;
 
 /**
@@ -9,10 +16,20 @@ import uncon.lv.uniapp.R;
  */
 public class CosplayActivity extends YberActivity {
 
+    HashMap<String, List<String>> cosplayersMap;
+    List<String> cosplayList;
+    ExpandableListView expList;
+    CosplayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cosplay);
+        expList = (ExpandableListView) findViewById(R.id.cosplay_exp_list);
+        cosplayersMap = CosplayDataProvider.getInfo();
+        cosplayList = new ArrayList<>(cosplayersMap.keySet());
+        adapter = new CosplayAdapter(this, cosplayersMap, cosplayList);
+        expList.setAdapter(adapter);
         setMenuButton();
     }
 }
